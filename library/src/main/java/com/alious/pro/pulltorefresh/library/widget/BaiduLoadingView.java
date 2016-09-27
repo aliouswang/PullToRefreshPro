606be173e37622e7f0b9ee61cc4752c498977cb2
aliouswang <wsw622@126.com>
2016-09-27 18:01:02 +0800

    add baidu loading style

--- library/src/main/java/com/alious/pro/pulltorefresh/library/widget/BaiduLoadingView.java
+++ library/src/main/java/com/alious/pro/pulltorefresh/library/widget/BaiduLoadingView.java
@@ -1,9 +1,14 @@
 package com.alious.pro.pulltorefresh.library.widget;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.alious.pro.pulltorefresh.library.R;
 import com.alious.pro.pulltorefresh.library.interfaces.IPercentView;
 
 /**
@@ -13,6 +18,14 @@ import com.alious.pro.pulltorefresh.library.interfaces.IPercentView;
  */
 
 public class BaiduLoadingView extends View implements IPercentView{
+
+    private Paint mPaint;
+    private Context mContext;
+
+    private Bitmap mLogoBitmap;
+    private Bitmap mWheelBitmap;
+    private Bitmap mSunBitmap;
+    private Bitmap mBackBitmap;
 
     public BaiduLoadingView(Context context) {
         this(context, null);
@@ -24,6 +37,30 @@ public class BaiduLoadingView extends View implements IPercentView{
 
     public BaiduLoadingView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mContext = context;
+        initView();
+    }
+
+    private void initView() {
+        mPaint = new Paint();
+        initBitmaps();
+    }
+
+    private void initBitmaps() {
+        mLogoBitmap = BitmapFactory.decodeResource(mContext.getResources(),
+                R.drawable.pull_rider);
+        mWheelBitmap = BitmapFactory.decodeResource(mContext.getResources(),
+                R.drawable.pull_wheel);
+        mSunBitmap = BitmapFactory.decodeResource(mContext.getResources(),
+                R.drawable.pull_sun);
+        mBackBitmap = BitmapFactory.decodeResource(mContext.getResources(),
+                R.drawable.pull_back);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
     }
 
     @Override
